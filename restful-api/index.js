70d9d78845b65caa400e72b5d970a3c3d5015f1a
Olufayo Jeremiah <olufayojeremiah@gmail.com>
2019-01-20 00:17:35 +0100

    Routing requests

--- restful-api/index.js
+++ restful-api/index.js
@@ -34,21 +34,58 @@
    req.on('end', function() {
      buffer += decoder.end();
 
-     // Send the response
-     res.end('Hello World\n');
-  
-     const output = `
-                    Request received on path: ${trimmedPath}
-                    with method: ${method}
-                    and with these query string parameters ${JSON.stringify(queryStringObject)}
-                    with these headers ${headers}
-                    `
-  
-     // Log the request path
-    console.log(`Request received with these payload ${buffer}`);
+     // Choose the router this request should go to. If one is not found, use the notFound handler
+     const chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
+
+     // Construct the data object to send to the handler
+     const data = {
+       'trimmedPath': trimmedPath,
+       'queryStringObject': queryStringObject,
+       'method': method,
+       'headers': headers,
+       'payload': buffer
+     };
+
+     // Route the request to the handler specified in the router
+     chosenHandler(data, function(statusCode, payload) {
+       // Use the status code called back by the handler, or default to 200
+      statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
+
+       // Use the payload called back by the handler, or default to an empty object
+       payload = typeof(payload) == 'object' ? payload : {};
+
+       // Convert the payload to a string
+       const payloadString = JSON.stringify(payload);
+
+       // Return the response
+       res.writeHead(statusCode);
+       res.end(payloadString);
+
+       // Log the request path
+       console.log(`Returning this response ${statusCode}, ${payloadString}`);
+     });
    });
  });
 
  server.listen(3000, function() {
    console.log('The server is listening on port 3000');
  });
+
+ // Define the handlers
+ const handlers = {};
+
+ // Sample handler
+ handlers.sample = function(data, callback) {
+   // Callback a http status code, and a payload object
+   callback(406, {'name': 'sample handler'});
+ };
+
+ // Not found handler
+ handlers.notFound = function(data, callback) {
+   callback(404);
+ };
+
+ // Define a request router
+ const router = {
+   'sample': handlers.sample
+ };
